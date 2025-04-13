761f9647c2684b57c6f6e01271a029611fabcd2f
Dominik Hoch <dominik.hoch.1989@freenet.de>
2025-04-13 11:22:12 +0200

    logging of keyboardinterruppt

--- autorun.py
+++ autorun.py
@@ -12,22 +12,24 @@ if __name__ == "__main__":
         level=logging.ERROR,       # Log level
         format='%(asctime)s - %(levelname)s - %(message)s'  # Log format
     )
-
-    DR = Display_Routine()
-    NR = Network_Routine()
-    DR.start()
-    NR.start()
-    sleep(10)
-
     try:
-        SR = Strip_Routine() 
-    except Exception as e:
-        logging.error("Error instantiate Strip_Routine: %s", str(e), exc_info=True)
+        DR = Display_Routine()
+        NR = Network_Routine()
+        DR.start()
+        NR.start()
+        sleep(10)
 
-    try:
-        SR.start()
-    except Exception as e:
-        logging.error("Error starting thread for Strip_Routine: %s", str(e), exc_info=True)    
-    
+        try:
+            SR = Strip_Routine() 
+        except Exception as e:
+            logging.error("Error instantiate Strip_Routine: %s", str(e), exc_info=True)
+
+        try:
+            SR.start()
+        except Exception as e:
+            logging.error("Error starting thread for Strip_Routine: %s", str(e), exc_info=True)    
+
+    except KeyboardInterrupt as k:
+            logging.error("Terminated via keyboard interrupt: %s", str(k))
 
 
